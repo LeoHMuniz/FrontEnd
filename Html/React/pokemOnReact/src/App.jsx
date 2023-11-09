2ef31f1c218ac0993b83d626f5a11ecd01d0e1ad
LeoM <leo-henriquesantos@hotmail.com>
2023-11-09 18:48:34 -0300

    modified:   src/App.jsx 	new file:   src/components/PokeTypes.jsx 	modified:   ../actuallyReact/src/components/Formulario.jsx 	modified:   ../actuallyReact/src/main.jsx 	modified:   ../strataOnReact/package-lock.json 	modified:   ../strataOnReact/package.json 	modified:   ../strataOnReact/src/components/Tables.jsx 	modified:   ../strataOnReact/src/components/Tables.module.css 	modified:   ../tryingReact/src/App.jsx 	modified:   ../tryingReact/src/components/App.module.css 	modified:   ../tryingReact/src/components/Sidebar.jsx 	modified:   ../tryingReact/src/global.css 	modified:   ../tryingReactDR/ProjetoDR/src/App.css 	modified:   ../tryingReactDR/ProjetoDR/src/App.jsx 	modified:   ../tryingReactDR/ProjetoDR/src/components/Card.module.css 	modified:   ../tryingReactDR/ProjetoDR/src/components/Carroussel.jsx 	modified:   ../tryingReactDR/ProjetoDR/src/components/Carroussel.module.css 	modified:   ../../Strata/index.html 	modified:   ../../Tailwind/Test/src/morty.js 	deleted:    ../../../Js/.gitignore

--- Html/React/pokemOnReact/src/App.jsx
+++ Html/React/pokemOnReact/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   function handleClick() {
     event.preventDefault()
-    getPokemon()
+    pokemonName === ""? alert('You gotta tell us a pokemon name'):getPokemon()
   }
 
 
@@ -22,8 +22,9 @@ function App() {
     const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
     setPokemon(await resposta.json())
     setCoin(true)
-    console.log(pokemon)
-    console.log(pokemon.types.slot.type)
+    console.log(pokemon.types.map(pokemonTypes => {
+      console.log(pokemonTypes.type.name)
+    }))
   }
 
   return (
@@ -33,7 +34,7 @@ function App() {
           <p>Welcome to my pokemOnReact!</p>
         </header>
         <main className='flex-col flex w-3/5 m-auto justify-center align-bottom text-center'>
-          <div className='justify-center align-middle text-center flex h-30'>
+          <div className='justify-center my-auto align-middle text-center flex h-30'>
             <img className='h-24 rounded-full bg-gray-100/50 m-4'
               src={coin ? pokemon.sprites.front_default : ""}
               alt=""
@@ -43,19 +44,24 @@ function App() {
           <input type="text" placeholder='Name of the pokemon' className='text-center p-2 rounded ' onChange={getPokemonName} id="nomePokemon" />
           <button type="submit" className='p-2 rounded bg-orange-400 w-3/5 m-auto my-4' htmlFor="nomePokemon" onClick={handleClick}>Click me</button>
         </main>
-      
-      {
-        coin ?
-        <div className="text-left mx-10 flex flex-col justify-center align-center">
-            <p>Nome do pokemon: {pokemon.name}</p>
-            <p>Habilidade principal: {pokemon.name}</p>
-          </div>
 
-: <div className="text-left mx-10 flex flex-col justify-center align-center">
-            <p>Nome do pokemon:</p>
-            <p>Habilidade principal:</p>
-          </div>
-      }
+
+        <div className="text-left mx-10 flex flex-col justify-center align-center">
+          {
+            coin ?
+              <p className='p-1'>Nome: {pokemon.name}</p>
+              : ""
+          }
+          {
+
+            coin ? pokemon.types.map(pokemonTypes => {
+              return (
+                <p  className='p-1'>Tipo: {pokemonTypes.type.name}</p>)
+            })
+              :
+              ""
+          }
+        </div>
       </div>
     </div>
 
@@ -63,3 +69,14 @@ function App() {
 }
 
 export default App
+
+//         coin ?
+//         <div className="text-left mx-10 flex flex-col justify-center align-center">
+//             <p>Nome do pokemon: {pokemon.name}</p>
+//             <p>Habilidade principal: {pokemon.name}</p>
+//           </div>
+
+// : <div className="text-left mx-10 flex flex-col justify-center align-center">
+//             <p>Nome do pokemon:</p>
+//             <p>Habilidade principal:</p>
+//           </div>
