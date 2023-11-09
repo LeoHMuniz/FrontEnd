0856e336c6c5764f0a2956282581efc0a906e37b
LeoM <leo-henriquesantos@hotmail.com>
2023-11-08 22:42:29 -0300

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/vite.svg 	new file:   src/App.jsx 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   tailwind.config.js 	new file:   vite.config.js
    Changes not staged for commit:
    	modified:   ../actuallyReact/src/components/Formulario.jsx
    	modified:   ../actuallyReact/src/main.jsx
    	modified:   ../strataOnReact/package-lock.json
    	modified:   ../strataOnReact/package.json
    	modified:   ../strataOnReact/src/components/Tables.jsx
    	modified:   ../strataOnReact/src/components/Tables.module.css
    	modified:   ../tryingReact/src/App.jsx
    	modified:   ../tryingReact/src/components/App.module.css
    	modified:   ../tryingReact/src/components/Sidebar.jsx
    	modified:   ../tryingReact/src/global.css
    	modified:   ../tryingReactDR/ProjetoDR/src/App.css
    	modified:   ../tryingReactDR/ProjetoDR/src/App.jsx
    	modified:   ../tryingReactDR/ProjetoDR/src/components/Card.module.css
    	modified:   ../tryingReactDR/ProjetoDR/src/components/Carroussel.jsx
    	modified:   ../tryingReactDR/ProjetoDR/src/components/Carroussel.module.css
    	modified:   ../../Strata/index.html
    	modified:   ../../Tailwind/Test/src/morty.js

---
+++ Html/React/pokemOnReact/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  const [coin, setCoin] = useState(false)
+  const [pokemon, setPokemon] = useState({})
+  const [pokemonName, setPokemonName] = useState('')
+
+  function getPokemonName() {
+    setPokemonName(nomePokemon.value.toLowerCase())
+  }
+
+  function handleClick() {
+    event.preventDefault()
+    getPokemon()
+  }
+
+
+
+  async function getPokemon() {
+    const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    setPokemon(await resposta.json())
+    setCoin(true)
+    console.log(pokemon)
+    console.log(pokemon.types.slot.type)
+  }
+
+  return (
+    <div className="bg-gray-600 h-screen w-full flex">
+      <div className='w-80 h-2/4 m-auto border justify-center border-gray-800 border-solid rounded bg-gray-400'>
+        <header className='mx-auto my-8 w-full text-center'>
+          <p>Welcome to my pokemOnReact!</p>
+        </header>
+        <main className='flex-col flex w-3/5 m-auto justify-center align-bottom text-center'>
+          <div className='justify-center align-middle text-center flex h-30'>
+            <img className='h-24 rounded-full bg-gray-100/50 m-4'
+              src={coin ? pokemon.sprites.front_default : ""}
+              alt=""
+            />
+          </div>
+
+          <input type="text" placeholder='Name of the pokemon' className='text-center p-2 rounded ' onChange={getPokemonName} id="nomePokemon" />
+          <button type="submit" className='p-2 rounded bg-orange-400 w-3/5 m-auto my-4' htmlFor="nomePokemon" onClick={handleClick}>Click me</button>
+        </main>
+      
+      {
+        coin ?
+        <div className="text-left mx-10 flex flex-col justify-center align-center">
+            <p>Nome do pokemon: {pokemon.name}</p>
+            <p>Habilidade principal: {pokemon.name}</p>
+          </div>
+
+: <div className="text-left mx-10 flex flex-col justify-center align-center">
+            <p>Nome do pokemon:</p>
+            <p>Habilidade principal:</p>
+          </div>
+      }
+      </div>
+    </div>
+
+  )
+}
+
+export default App
